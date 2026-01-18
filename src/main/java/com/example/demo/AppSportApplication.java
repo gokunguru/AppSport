3a47ce46f291c6e0bca0e3f8648f0b167864fa8d
mandi <mandi@et.esiea.fr>
2026-01-18 16:41:56 +0100

    Initial commit - AppSport backend

--- src/main/java/com/example/demo/AppSportApplication.java
+++ src/main/java/com/example/demo/AppSportApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class AppSportApplication {
 
 	public static void main(String[] args) {
